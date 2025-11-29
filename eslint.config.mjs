ddba885b1e78476afe2d07350fea54dde21e5db7
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2025-11-29 17:35:10 +0200

    chore: config prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...pluginQuery.configs["flat/recommended"],
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  // Disable any conflict between ESLint and Prettier
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
