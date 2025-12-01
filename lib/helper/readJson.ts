808e8df29d6cb39e8627bbe3c2960118d791b26a
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2025-12-01 02:51:03 +0200

    refactor: removed unneeded files after converting to supabase

--- lib/helper/readJson.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import fs from "fs/promises";
-
-export async function readJson<T>(file: string): Promise<T> {
-  const filePath = path.join(process.cwd(), "db", file);
-  const data = await fs.readFile(filePath, "utf-8");
-  return JSON.parse(data);
-}
